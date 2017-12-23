195b4bcc66bfefa9bafe874117051c69b75d2db9
angpao <angpao.kittimasak>
2017-12-23 18:41:52 +0700

    swap box3 to box4 change box3 to sweet alert

--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,33 @@
 function sweet() {
     swal("Good job!", "You clicked the button!", "success");
 }
+
+function wrt() {
+    swal({
+        title: '1 + 1 = ?',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '2',
+        cancelButtonText: '3',
+        buttonsStyling: false,
+        reverseButtons: true
+      }).then((result) => {
+        if (result.value) {
+          swal(
+            'คำตอบถูกต้อง!',
+            '',
+            'success'
+          )
+        // result.dismiss can be 'cancel', 'overlay',
+        // 'close', and 'timer'
+        } else if (result.dismiss === 'cancel') {
+          swal(
+            'คำตอบผิด!',
+            '',
+            'error'
+          )
+        }
+      })
+}
