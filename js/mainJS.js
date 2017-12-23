3b266dff0a71bed8c109c501ffe85dda09f3201e
angpao <angpao.kittimasak>
2017-12-23 17:25:19 +0700

    add box2 with sweet alert

---
+++ js/mainJS.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $("#link1").click(function () {
+        $path = $("#box1").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link2").click(function () {
+        $path = $("#box2").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link3").click(function () {
+        $path = $("#box3").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link4").click(function () {
+        $path = $("#box4").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link5").click(function () {
+        $path = $("#box5").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link6").click(function () {
+        $path = $("#box6").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link7").click(function () {
+        $path = $("#box7").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link8").click(function () {
+        $path = $("#box8").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link9").click(function () {
+        $path = $("#box9").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+    $("#link10").click(function () {
+        $path = $("#box10").offset().top;
+        $('body').animate({ scrollTop: $path }, 1000);
+    });
+});
