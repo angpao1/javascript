d081ee9cbe1e999d00a1279057fb36d54a1144d7
angpao <angpao.kittimasak>
2017-12-24 10:47:26 +0700

    finish box7 and box8

--- js/alert.js
+++ js/alert.js
@@ -1,65 +1,66 @@
 function sweet() {
-    swal("Good job!", "You clicked the button!", "success");
+  swal("Good job!", "You clicked the button!", "success");
 }
 
 function wrt() {
-    swal({
-        title: '1 + 1 = ?',
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '2',
-        cancelButtonText: '3',
-        buttonsStyling: false,
-        reverseButtons: true
-      }).then((result) => {
-        if (result.value) {
-          swal(
-            'คำตอบถูกต้อง!',
-            '',
-            'success'
-          )
-        // result.dismiss can be 'cancel', 'overlay',
-        // 'close', and 'timer'
-        } else if (result.dismiss === 'cancel') {
-          swal(
-            'คำตอบผิด!',
-            '',
-            'error'
-          )
-        }
-      })
+  swal({
+    title: '10 + 5 = ?',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: '15',
+    cancelButtonText: '30',
+    buttonsStyling: false,
+    reverseButtons: true
+  }).then((result) => {
+    if (result.value) {
+      swal(
+        'คำตอบถูกต้อง!',
+        '',
+        'success'
+      )
+      // result.dismiss can be 'cancel', 'overlay',
+      // 'close', and 'timer'
+    } else if (result.dismiss === 'cancel') {
+      swal(
+        'คำตอบผิด!',
+        '',
+        'error'
+      )
+    }
+  })
 }
 
 function emi() {
-    swal({
-        title: 'กรอกอีเมล์ที่ต้องการส่ง',
-        input: 'email',
-        showCancelButton: true,
-        confirmButtonText: 'ส่ง',
-        cancelButtonText: 'ยกเลิก',
-        showLoaderOnConfirm: true,
-        preConfirm: (email) => {
-          return new Promise((resolve) => {
-            setTimeout(() => {
-              if (email === 'taken@example.com') {
-                swal.showValidationError(
-                  'This email is already taken.'
-                )
-              }
-              resolve()
-            }, 2000)
-          })
-        },
-        allowOutsideClick: false
-      }).then((result) => {
-        if (result.value) {
-          swal({
-            type: 'success',
-            title: 'ส่งข้อมูลสำเร็จ!',
-            html: 'ส่งข้อมูลไป: ' + result.value
-          })
-        }
+  swal({
+    title: 'กรอกอีเมล์ที่ต้องการส่ง',
+    input: 'email',
+    showCancelButton: true,
+    confirmButtonText: 'ส่ง',
+    cancelButtonText: 'ยกเลิก',
+    showLoaderOnConfirm: true,
+    preConfirm: (email) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          if (email === 'taken@example.com') {
+            swal.showValidationError(
+              'This email is already taken.'
+            )
+          }
+          resolve()
+        }, 2000)
       })
+    },
+    allowOutsideClick: false
+  }).then((result) => {
+    if (result.value) {
+      swal({
+        type: 'success',
+        title: 'ส่งข้อมูลสำเร็จ!',
+        html: 'ส่งข้อมูลไป: ' + result.value
+      })
+    }
+  })
 }
+
